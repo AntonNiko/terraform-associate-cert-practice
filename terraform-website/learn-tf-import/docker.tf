e4dd0653f7784f5c03bd141a9370e8014762e250
Anton Nikitenko <ant.nikitenko@gmail.com>
2025-10-06 21:22:44 -0700

    Add learn terraform state code

--- terraform-website/learn-tf-import/docker.tf
+++
@@ -1,9 +0,0 @@
-# Copyright (c) HashiCorp, Inc.
-# SPDX-License-Identifier: MPL-2.0
-
-# Terraform configuration
-
-import {
-  id = "036769c951b5717a7352f561fcc04f5386ee2351f491d01f8679fe5043353a53"
-  to = docker_container.web
-}
